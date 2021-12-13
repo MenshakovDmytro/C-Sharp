c945def89d670327a0a4ee367b4e0de9d66aa6cb
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-13 12:41:35 +0200

    added Lab08

---
+++ menshakov08/menshakov08/Migrations/202112071256435_SomeTest2.cs
@@ -0,0 +1,16 @@
+namespace menshakov08.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SomeTest2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
